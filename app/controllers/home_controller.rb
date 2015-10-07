7ac63251ba436a1760736a6c10cd063d25f35b77
David Bernheisel <david@bernheisel.com>
2015-10-07 11:00:48 -0400

    Challenge 10-7

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,49 @@
 class HomeController < ApplicationController
+  before_action :set_home, only: [:show]
 
-  def form
-    if request.post?
-      @answer = Answer.create!(
-            name: params[:answer][:name],
-            chocolate: params[:answer][:chocolate],
-            puppies: params[:answer][:puppies],
-            cold_hard_cash: params[:answer][:cold_hard_cash],
-            rainbows: params[:answer][:rainbows]
-      )
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.create!(answer_params)
+
+    if @answer.save
+      redirect_to @answer, notice: 'SUCCCESSS!!'
     else
-      @answer = Answer.new
+      render :new
     end
   end
+
+  def show
+
+  end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:name, :chocolate, :puppies, :rainbows, :cold_hard_cash)
+    end
+
+    def home
+      @answer = Answer.find(params[:id])
+    end
+
+  # def login
+  #   e = params[:email]
+  #   pw = params[:password]
+  #   u = User.find_by_email(e)
+
+  #   if u.authenticate(pw)
+  #     session[:something] = #something
+  #     redirect_to happy_path, notice: "Success Login"
+  #   else
+  #     redirect_to bad_path, notice: "Bad login info"
+  #   end
+  # end
+
+  # def logout
+  #   session[:something] = nil
+  #   redirect_to happy_path, notice: "Success Login"
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'answers/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#form'
-  get 'home/form'
-  post 'home/form'
+  root 'home#new'
+  get 'home/new'
+  post 'home/create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
